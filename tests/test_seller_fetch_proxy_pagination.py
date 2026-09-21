45e5935133a92296c42093543c078f91e49331e2
rigGihHub <rikardekstrom@yahoo.com>
2026-09-21 08:19:05 +0200

    Add regression tests for seller cursor pagination

---
+++ tests/test_seller_fetch_proxy_pagination.py
@@ -0,0 +1,21 @@
+from seller_fetch_proxy import _PAGE_URL_CACHE, _cache_key, _page_href, _safe_tradera_page_url
+
+
+def test_real_tradera_paging_token_is_extracted():
+    html = '<a aria-label="Sida 10" href="/profile/items/123/foo?paging=10.a0.s9529">10</a>'
+    assert _page_href(html, 10) == "/profile/items/123/foo?paging=10.a0.s9529"
+
+
+def test_page_cursor_cache_distinguishes_seller_alias_and_page():
+    _PAGE_URL_CACHE.clear()
+    key10 = _cache_key(123, "Etanol71", 10)
+    key11 = _cache_key(123, "Etanol71", 11)
+    _PAGE_URL_CACHE[key10] = "https://www.tradera.com/profile/items/123/Etanol71?paging=10.a0.s9529"
+    assert _PAGE_URL_CACHE[key10].endswith("paging=10.a0.s9529")
+    assert key11 not in _PAGE_URL_CACHE
+
+
+def test_only_tradera_profile_urls_are_accepted_as_cached_cursors():
+    assert _safe_tradera_page_url("https://www.tradera.com/profile/items/123/foo?paging=11.a0.s9529")
+    assert not _safe_tradera_page_url("https://evil.example/profile/items/123/foo?paging=11.a0.s9529")
+    assert not _safe_tradera_page_url("https://www.tradera.com/item/123/456")
